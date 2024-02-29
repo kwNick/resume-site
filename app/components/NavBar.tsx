adf7a0c0bef6a29cf0eee1ad9ca0785f6e13b4e7
Nickolas Piraino <kwnp17c@gmail.com>
2024-02-28 22:17:40 -0500

    making more components and styles

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
+import NavItem from "./NavItem";
 export default function NavBar(){
     return (
-        <div className="pt-5 flex items-center justify-center">
-            <ul className="px-4 py-3 hidden bg-slate-400 md:inline-flex gap-x-5 border rounded-xl uppercase ">
-               <Link href="/"><li className="text-slate-200">Resume</li></Link>
-               <li>Experience</li>
-               <li>Publications</li>
-               <li>Links</li> 
-            </ul>
-        </div>
+            <div className="pt-5 flex items-center justify-center">
+                <NavItem/>
+                <ul className="px-8 py-7 hidden text-2xl bg-slate-400 md:inline-flex self-center gap-x-7 border rounded-xl uppercase border-slate-900 ">
+                    <Link href="/"><li className="text-slate-200 font-bold">Resume</li></Link>
+                    <li>Experience</li>
+                    <li>Publications</li>
+                    <li>Links</li> 
+                </ul>
+            </div>
     );
 }
