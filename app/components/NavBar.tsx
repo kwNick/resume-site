904c1759b7e803f90a92a41b9c4515327cfd09da
Nickolas Piraino <kwnp17c@gmail.com>
2024-02-29 01:13:36 -0500

    styling header with constant list array, and hover effects, contact button

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,21 +5,16 @@ import NavListItem from "./NavListItem";
 export default function NavBar(){
     return (
             <div className="h-24 px-10 flex items-center justify-between">
-                <Link href="/" className="font-extrabold text-3xl"><span>Nick</span></Link>
-                {/* <NavLogo/> */}
-                <ul className="px-8 py-7 hidden text-2xl bg-slate-400 md:inline-flex self-center gap-x-7 border rounded-xl uppercase border-slate-900 ">
+                <NavLogo/>
+                <ul className="px-6 py-4 bg-slate-400 text-xl font-medium md:inline-flex gap-x-7 border border-green-300 rounded-full uppercase border-slate-900 ">
                     {
-                        // navList.map({title, href}) =>(
-                        //     <NavListItem key={title} title={title} href={href}/>
-                        // )
+                        navList.map(({title, href}) =>(
+                            <NavListItem key={title} title={title} href={href}/>
+                        ))
                     }
-                    <Link href="/"><li className="text-slate-200 font-bold">Resume</li></Link>
-                    <li>Experience</li>
-                    <li>Publications</li>
-                    <li>Links</li> 
                 </ul>
-                <div>
-                    <h1>Contact Me!</h1>
+                <div className="">
+                    <button className="border rounded-xl px-5 py-3 cursor-pointer uppercase text-xl font-medium hover:bg-slate-200 hover:text-green-800 duration-300">Contact Me!</button>
                 </div>
             </div>
     );
--- app/components/NavListItem.tsx
+++ app/components/NavListItem.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
-// interface Props = {
-//     title: string;
-    
-// };
-export default function NavListItem(){
+interface Props {
+    title: string;
+    href:string;    
+};
+export default function NavListItem({title, href}: Props){
     return (
-        <Link href="/"><li className="text-slate-200 font-bold">Resume</li></Link>
+        <Link href={href}>
+            <li key={title}className="text-slate-200 hover:text-green-300 duration-200">{title}</li>
+            </Link>
     );
 }
