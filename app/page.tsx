723a0e93664d80dc1b15ca83a1f9d1ea4c13817e
Nickolas Piraino <kwnp17c@gmail.com>
2024-02-28 21:14:45 -0500

    some css

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div><h1>Hello World!</h1></div>
+    <div className="flex items-center justify-center pt-3 ">
+      <h1>Hello World!</h1>
+    </div>
   );
 }
