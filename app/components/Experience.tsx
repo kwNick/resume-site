17bef9800fb05de077871263f368d0ef2f5a8f0d
Nickolas Piraino <kwnp17c@gmail.com>
2024-03-03 04:47:45 -0500

    finishing state components for now

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,7 +1,14 @@
 export default function Experience(){
     return(
-        <div>
+        <div className="m-auto max-w-2xl flex flex-col items-center justify-center mt-10 gap-y-7 p-10 border rounded-xl">
             <h1>Experience Page!</h1>
+            <div className="flex flex-col items-center justify-center">Researcher
+                Supervised by Michael https://www.mgubanov.com Gubanov | June 2021 Dec. 2023 | Department Computer Science, FSU | 1017 Academic Way, Tallahassee, FL
+                https://www.covidkg.org Focused Project: covidkg.org
+                Experience in an experimental startup, where I was the team lead of up to 8 other collaborators working daily. Oversaw each individuals role and progression towards creating a trustworthy, up-to-date medical resource for any audience. Our team successfully completed NSF I-corps innovation regional and national program, received funding from Amazon.
+                Manually configuring and troubleshooting 6 physical hardware mounted rack servers. Integrated Amazon web Services S3 buckets and custom EC2 instances. Attributed to the front-end(Bootstrap, JQuery D3JS, Observablehq, Apache echarts, phpMongo Driver), back-end(mongod) and machine learning(tensorflow, spark) requirements of our system. 
+                Determined user requirements alongside Dr. Gubanov, to design and implement new code or modify existing code. Held weekly team meetings to discuss our results.
+            </div>
         </div>
     );
 }
--- app/components/Publications.tsx
+++ app/components/Publications.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
 export default function Publications(){
     return (
         <div className="mt-10 p-8 m-auto max-w-xl flex flex-col gap-y-8 items-center justify-center border bg-gray-500 text-slate-900 rounded-3xl border-green-300">
-            <h1>Hybrid Metadata Classification in Large-scale Structured Data sets"
-            Sophie Pavia, Nick Piraino, Kazi Islam, Anna Pyayt, Michael Gubanov, invited paper in the journal of Data Intelligence, Rinton Press, Special Issue on "Best of DEXA", 2021</h1>
+            <h1><Link className="underline" href="https://link.springer.com/chapter/10.1007/978-3-030-86472-9_4" target="_blank">Hybrid Metadata Classification in Large-scale Structured Data sets"</Link>
+             Sophie Pavia, Nick Piraino, Kazi Islam, Anna Pyayt, Michael Gubanov, invited paper in the journal of Data Intelligence, Rinton Press, Special Issue on "Best of DEXA", 2021</h1>
 
-            <h1>COVIDKG.ORG - a Web-scale COVID-19 Interactive, Trustworthy Knowledge Graph, Constructed and Interrogated for Bias using Deep-Learning"
-            Bhim Kandibedala, Anna Pyayt, Nick Piraino, Chris Caballero, Michael Gubanov, to appear in EDBT, 2023"</h1>
+            <h1><Link className="underline" href="https://openproceedings.org/2023/conf/edbt/3-paper-147.pdf" target="_blank">COVIDKG.ORG - a Web-scale COVID-19 Interactive, Trustworthy Knowledge Graph, Constructed and Interrogated for Bias using Deep-Learning"</Link>
+             Bhim Kandibedala, Anna Pyayt, Nick Piraino, Chris Caballero, Michael Gubanov, appeared in EDBT, 2023"</h1>
         </div>
     );
 }
